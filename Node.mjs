325b2bc9c24ff8357c6961bff17d7b13e6795c4a
cadltop <caesarfromthecross.808@gmail.com>
2024-10-29 09:11:15 -0500

    Set initial classes

---
+++ Node.mjs
@@ -0,0 +1,7 @@
+export default class {
+    constructor(data, leftChild, rightChild) {
+        this.data = data,
+        this.leftChild = leftChild,
+        this.rightChild = rightChild
+    }
+}
