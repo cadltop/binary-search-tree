688fb7707c2ac6c79d7278bb512c0d819270b3d8
cadltop <caesarfromthecross.808@gmail.com>
2024-12-11 20:17:30 -0500

    Test the Tree class

---
+++ main.mjs
@@ -0,0 +1,18 @@
+import Tree from "./Tree.mjs";
+
+const tree = new Tree([1, 3, 8, 3, 5, 7, 9, 6, 1, 5]);
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+    if (node === null) {
+      return;
+    }
+    if (node.rightChild !== null) {
+      prettyPrint(node.rightChild, `${prefix}${isLeft ? "│   " : "    "}`, false);
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.leftChild !== null) {
+      prettyPrint(node.leftChild, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+};
+
+prettyPrint(tree.root);
