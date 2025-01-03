c4d1bb822ae90c64896af00860572143e68ee31b
cadltop <caesarfromthecross.808@gmail.com>
2025-01-03 00:30:26 -0500

    set buildTree method and test.

--- Tree.mjs
+++ Tree.mjs
@@ -1,3 +1,4 @@
+import Node from './Node.mjs';
 export default class {
     constructor(data) {
         this.root = this.buildTree(data);
@@ -5,8 +6,27 @@ export default class {
     buildTree(data) {
         const cleanData = this.#clean(data);
         const sortedData = this.#sort(cleanData);
+        return this.#build(sortedData);
     }
+    #build(data = []) {
+        if (data[0] === undefined) return null;
+        if (Math.floor(data.length / 2) < 1) return new Node(data[0]);
 
+        const middle = Math.floor(data.length / 2);
+        let left = [], right = [];
+
+        for (let i = 0; i < middle; i++) {
+            left.push(data[i]);
+        }
+        for (let i = middle + 1; i < data.length; i++) {
+            right.push(data[i]);
+        }
+
+        const treeNode = new Node(data[middle]);
+        treeNode.leftChild = this.#build(left);
+        treeNode.rightChild = this.#build(right);
+        return treeNode;
+    }
     #clean(data = []) {
         const cleanData = [];
         
@@ -30,33 +50,33 @@ export default class {
         if (data.length === 1) return data;
 
         const middle = Math.floor(data.length / 2);
-        let left = [], rigth = [];
+        let left = [], right = [];
 
         for (let i = 0; i < middle; i++) {
             left.push(data[i]);
         }
         for (let i = middle; i < data.length; i++) {
-            rigth.push(data[i]);
+            right.push(data[i]);
         }
 
         left = this.#sort(left);
-        rigth = this.#sort(rigth);
+        right = this.#sort(right);
         
         let merged = [];
         for (let i = 0; i < data.length; i++) {
             if (left[0] === undefined) {
-                rigth.forEach((value) => {
+                right.forEach((value) => {
                     merged.push(value)
                 })
                 break;
-            } else if (rigth[0] === undefined) {
+            } else if (right[0] === undefined) {
                 left.forEach((value) => {
                     merged.push(value)
                 })
                 break;
             } else {
-                if (left[0] < rigth[0]) merged.push(left.shift());
-                else merged.push(rigth.shift());
+                if (left[0] < right[0]) merged.push(left.shift());
+                else merged.push(right.shift());
             }
         }
         return merged;
