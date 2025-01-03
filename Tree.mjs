0e5c0240b61a749d547bd9bdbc7079a12b10f1fb
cadltop <caesarfromthecross.808@gmail.com>
2025-01-03 14:46:57 -0500

    set insert method

--- Tree.mjs
+++ Tree.mjs
@@ -4,28 +4,26 @@ export default class {
         this.root = this.buildTree(data);
     }    
     buildTree(data) {
-        const cleanData = this.#clean(data);
-        const sortedData = this.#sort(cleanData);
-        return this.#build(sortedData);
+        data = this.#clean(data);
+        data = this.#sort(data);
+        return this.#build(data);
     }
-    #build(data = []) {
-        if (data[0] === undefined) return null;
-        if (Math.floor(data.length / 2) < 1) return new Node(data[0]);
+    insert(value) {
+        traverse(this.root, value);
+        function traverse(currentNode, val) {
+            if (currentNode === null) {
+                return new Node(val);
+            }
+            if (currentNode.data === val) {
+                console.log("this value exists already.");
+                return currentNode;
+            }
 
-        const middle = Math.floor(data.length / 2);
-        let left = [], right = [];
-
-        for (let i = 0; i < middle; i++) {
-            left.push(data[i]);
+            if (val < currentNode.data) currentNode.leftChild = traverse(currentNode.leftChild, val);
+            if (val > currentNode.data) currentNode.rightChild = traverse(currentNode.rightChild, val);
+            
+            return currentNode;
         }
-        for (let i = middle + 1; i < data.length; i++) {
-            right.push(data[i]);
-        }
-
-        const treeNode = new Node(data[middle]);
-        treeNode.leftChild = this.#build(left);
-        treeNode.rightChild = this.#build(right);
-        return treeNode;
     }
     #clean(data = []) {
         const cleanData = [];
@@ -48,37 +46,56 @@ export default class {
     }
     #sort(data = []) {
         if (data.length === 1) return data;
-
+        
         const middle = Math.floor(data.length / 2);
         let left = [], right = [];
-
+        
         for (let i = 0; i < middle; i++) {
             left.push(data[i]);
         }
         for (let i = middle; i < data.length; i++) {
             right.push(data[i]);
         }
-
+        
         left = this.#sort(left);
         right = this.#sort(right);
         
-        let merged = [];
+        const sortedData = [];
         for (let i = 0; i < data.length; i++) {
             if (left[0] === undefined) {
                 right.forEach((value) => {
-                    merged.push(value)
+                    sortedData.push(value)
                 })
                 break;
             } else if (right[0] === undefined) {
                 left.forEach((value) => {
-                    merged.push(value)
+                    sortedData.push(value)
                 })
                 break;
             } else {
-                if (left[0] < right[0]) merged.push(left.shift());
-                else merged.push(right.shift());
+                if (left[0] < right[0]) sortedData.push(left.shift());
+                else sortedData.push(right.shift());
             }
         }
-        return merged;
+        return sortedData;
+    }
+    #build(data = []) {
+        if (data[0] === undefined) return null;
+        if (Math.floor(data.length / 2) < 1) return new Node(data[0]);
+    
+        const middle = Math.floor(data.length / 2);
+        let left = [], right = [];
+    
+        for (let i = 0; i < middle; i++) {
+            left.push(data[i]);
+        }
+        for (let i = middle + 1; i < data.length; i++) {
+            right.push(data[i]);
+        }
+    
+        const treeNode = new Node(data[middle]);
+        treeNode.leftChild = this.#build(left);
+        treeNode.rightChild = this.#build(right);
+        return treeNode;
     }
 }
--- main.mjs
+++ main.mjs
@@ -1,6 +1,12 @@
 import Tree from "./Tree.mjs";
 
 const tree =  new Tree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+
+// insert
+// tree.insert(1000);
+// tree.insert(9);
+
+// print
 const prettyPrint = (node, prefix = "", isLeft = true) => {
     if (node === null) {
       return;
